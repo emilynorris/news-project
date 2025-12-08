2dc0eb6abebf1ef967d4c86595594451b356a342
emilynorris <emilynorris2303@gmail.com>
2025-12-08 13:38:57 +0000

    Created /api/users endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,40 +4,69 @@ const data = require('../db/data/test-data/index');
 const app = require("../app.js");
 const db = require('../db/connection');
 
-beforeEach (() => seed(data));
+beforeAll (() => seed(data));
 afterAll(() => db.end());
 
 describe('GET /api/topics', () => {
-  test('responds with array of objects containing slug and description of all topics', () => {
+  test('responds with array of objects containing all topics', () => {
     return request(app)
       .get('/api/topics')
       .expect(200)
       .then (({body}) => {
-        expect(body.topics.length).toBe(3)
-        expect(typeof body.topics[0].slug).toBe("string")
-        expect(typeof body.topics[1].description).toBe("string")
+        expect(body.topics.length).toBeGreaterThan(0)
+        body.topics.forEach((topic) => {
+          expect(typeof topic.slug).toBe("string")
+          expect(typeof topic.description).toBe("string")
+        })
       })
-  });
-  test('responds with array of objects for all articles' , () => {
+    })
+  })
+
+  describe('/api/articles', () => {
+  test('GET: 200 - responds with array of objects for all articles' , () => {
     return request(app)
       .get('/api/articles')
       .expect(200)
       .then (({body}) => {
-        expect(body.articles.length).toBe(13)
-
-        expect(typeof body.articles[9].author).toBe("string")
-        expect(typeof body.articles[1].title).toBe("string")
-        expect(typeof body.articles[2].article_id).toBe("number")
-        expect(typeof body.articles[3].topic).toBe("string")
-        expect(typeof body.articles[4].created_at).toBe("string")
-        expect(typeof body.articles[5].votes).toBe("number")
-        expect(typeof body.articles[6].article_img_url).toBe("string")
-        expect(typeof body.articles[7].comment_count).toBe("number")
-
+        expect(body.articles.length).toBeGreaterThan(0)
+        body.articles.forEach((article) => {
+          expect(typeof article.author).toBe("string")
+          expect(typeof article.title).toBe("string")
+          expect(typeof article.article_id).toBe("number")
+          expect(typeof article.topic).toBe("string")
+          expect(typeof article.created_at).toBe("string")
+          expect(typeof article.votes).toBe("number")
+          expect(typeof article.article_img_url).toBe("string")
+          expect(typeof article.comment_count).toBe("number")
+        })
         expect(body.articles[8]).not.toHaveProperty('body')
-
         expect(new Date (body.articles[0].created_at)).toEqual(new Date(1604394720000))
         expect(new Date (body.articles[12].created_at)).toEqual(new Date (1578406080000))
       })
-  });
-});
+  })
+  test('GET: 200 - takes optional query to filter result for articles', () => {
+    return request(app)
+      .get('/api/articles/5')
+      .expect(200)
+      .then (({body}) => {
+        expect(body.article[0].article_id).toBe(5)
+        expect(body.article.length).toBe(1)
+      })
+  })
+})
+
+describe ('GET /api/users', () => {
+  test('responds with array of objects containing all users', () => {
+    return request(app)
+      .get('/api/users')
+      .expect(200)
+      .then (({body}) => {
+        expect(body.users.length).toBeGreaterThan(0)
+        body.users.forEach((user) => {
+          expect(typeof user.username).toBe("string")
+          expect(typeof user.name).toBe("string")
+          expect(typeof user.avatar_url).toBe("string")
+        })  
+      })
+  })
+})
--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const db = require("./db/connection");
 
-app.get("/api/topics", (request, response) => {
-    return db.query(`SELECT slug, description FROM topics;`)
+app
+.get("/api/topics", (request, response) => {
+    return db.query(`
+        SELECT 
+        slug, 
+        description 
+        FROM topics;
+        `)
     .then (({ rows }) => {
+        // console.log ("topics", rows)
         response.status(200).send({ topics: rows})
     })
 })
@@ -24,11 +31,34 @@ app.get("/api/articles", (request, response) => {
         LEFT JOIN comments c
         ON a.article_id = c.article_id
         GROUP BY a.article_id, a.author
-        ORDER BY a.created_at DESC;`)
+        ORDER BY a.created_at DESC;
+        `)
     .then (({ rows }) => {
-        console.log("articles", rows)
+        // console.log("articles", rows)
         response.status(200).send({ articles: rows})
     })
 })
+
+app.get("/api/articles/:article_id", (request, response) => {
+    const { article_id } = request.params
+    return db.query(`
+        SELECT * FROM articles
+        WHERE article_id = ${article_id}
+        `)
+    .then (({ rows }) => {
+        console.log("article", rows)
+        response.status(200).send({ article: rows})
+    })
+})
+
+app.get("/api/users", (request,response) => {
+    return db.query(`SELECT * FROM users;`)
+    .then (({ rows }) => {
+        response.status(200).send({ users: rows})
+    })
+})
+
+
+
 
 module.exports = app
